8fe746385856fe4e8230d4f4e42176a2e935bda8
Fomin <vyacheslav.fomin@magenta-technology.com>
2022-05-06 09:41:26 +0400

    lab1 test by tasks

--- src/main/kotlin/lab1/Lab1.kt
+++ src/main/kotlin/lab1/Lab1.kt
@@ -1,27 +1,78 @@
 package lab1
 
 import jetbrains.letsPlot.export.ggsave
-import jetbrains.letsPlot.ggplot
 import space.kscience.kmath.complex.ComplexField.exp
 import space.kscience.kmath.complex.ComplexField.i
 import space.kscience.kmath.complex.ComplexField.times
 import kotlin.math.exp
 import kotlin.math.pow
 
-var B = 0.1
-const val A = 1
-val xRange = DoubleRange(-5.0, 5.0, 1000)
-val ksiRange = DoubleRange(-25.0, 25.0, 1000)
+var β = 0.1
+var α = 1.0
+var c = 5.0
+var p = -25.0
+var q = 25.0
+var n = 1000
+var m = 1000
 
-val f = { x: Double -> exp(i * B * x) }
+const val X_LABEL = "x"
+const val KSI_LABEL = "ξ"
+const val FX_LABEL = "f($X_LABEL)"
+const val FKSI_LABEL = "F($KSI_LABEL)"
+const val EXT = ".png"
+
+val f = { x: Double -> exp(i * β * x) }
 
 val kor = { ksi: Double, x: Double ->
-    i * exp(-A * (x - ksi).pow(2))
+    i * exp(-α * (x - ksi).pow(2))
 } // Fomin: variant 12
 
 fun main() {
-    f.integralTransform(ksiRange, xRange, kor).run {
-        println(ggsave(ggplot(ksiRange to args()), "Args of f default.png"))
-        println(ggsave(ggplot(ksiRange to absolutes()), "Absolutes of f default.png"))
-    }
+    // Task 1
+    listOf(0.1, 100.0, 0.001, -0.1, -0.001).forEach(::plotDefaultFToFile)
+
+    // Task 2
+    plotTransformToFile(p1 = p, q1 =q)
+
+    // Task 3
+    plotTransformToFile(p1 = p * 2, q1 = q * 2)
+    plotTransformToFile(p1 = p * 0.5, q1 = q * 0.5)
+
+    // Task 4
+    listOf(0.1, 100.0, 0.001).forEach { plotTransformToFile(a = it) }
+
+    // Task 5
+    listOf(c, 1.0, 100.0).forEach { plotTransformToFile(c1 = it) }
+
 }
+
+fun plotTransformToFile(a: Double = α, p1: Double = p, q1: Double = q, c1: Double = c) {
+    val prevA = α
+    α = a
+    val ksiRange = DoubleRange(p1, q1, m, KSI_LABEL)
+    f.integralTransform(ksiRange, DoubleRange(-c1, c1, n, X_LABEL), kor).run {
+        plotOnRange(ksiRange, args(), FKSI_LABEL,"Args of f transform (α = $α, p = $p1, q = $q1)")
+        plotOnRange(ksiRange, absolutes(), FKSI_LABEL,"Absolutes of f transform (α = $α, p = $p1, q = $q1)")
+    }
+    α = prevA
+}
+
+fun plotDefaultFToFile(b: Double) {
+    val prevB = β
+    β = b
+    val xRange = DoubleRange(-c, c, n, X_LABEL)
+    plotOnRange(xRange, f(xRange).args(), FX_LABEL, "Args of f default (β = $β)")
+    plotOnRange(xRange, f(xRange).absolutes(), FX_LABEL, "Absolutes of f default (β = $β)")
+    β = prevB
+}
+
+fun plotOnRange(xData: DoubleRange, yData: List<Double>, yLabel: String, title: String) =
+    println(
+        ggsave(
+            plot(
+                xData.to(yData, yLabel),
+                title
+            ),
+            "$title$EXT"
+        )
+    )
